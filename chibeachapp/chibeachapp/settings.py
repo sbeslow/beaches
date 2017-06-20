c43cab1cb1a05e0acbc59204cfc596a8f3c4dd89
sbeslow <bezzy21@yahoo.com>
2017-06-19 21:00:10 -0500

    Prepare for heroku

--- chibeachapp/chibeachapp/settings.py
+++ chibeachapp/chibeachapp/settings.py
@@ -115,8 +115,15 @@ USE_L10N = True
 
 USE_TZ = True
 
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.11/howto/static-files/
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+
+# Extra places for collectstatic to find static files.
+# STATICFILES_DIRS = (
+#     os.path.join(PROJECT_ROOT, 'static'),
+# )
+
+STATICFILES_DIRS = (
+
+    os.path.join(BASE_DIR, 'beach', 'static'),
+)
